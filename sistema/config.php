ed15b02789973948b9a38d4d10d0d2ceb0bc59f9
Gustavo <gfmb94@gmail.com>
2022-02-20 00:11:51 -0300

    Criando template da pagina de login e a conexão com o banco de dados

---
+++ sistema/config.php
@@ -0,0 +1,23 @@
+<?php
+//variaveis do projeto
+$url='http://localhost/restaurante/';
+$nome_site='Magalanches';
+$email_adm='gfmb94@gmail.com';
+$endereco_site='Rua x, Número 50, Bairro Centro - Salvador - BA CEP 89993-000';
+$telefone_fixo='(33) 3333-3333';
+$telefone_whatsapp='(33) 93333-3333';
+$telefone_whatsapp_link='5531933333333';
+
+//vairaveis para rede sociais
+
+$instagram='https://www.instagram.com/Magalanches/';
+$facebook='https://www.facebook.com/Magalanches/';
+$youtube='https://www.youtube.com/channel/Magalanches/';
+$twitter='https://www.google.com/';
+
+//variaveis para o banco de dados
+$servidor='localhost';
+$usuario='root';
+$senha=''
+$banco-'restaurante';
+?>
